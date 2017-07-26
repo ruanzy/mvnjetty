549b2c5f8a487830458051ec1929441403f707f7
ruanzy <ruanzhiyong6496@163.com>
2017-07-26 17:52:52 +0800

    优化

--- bin/start.bat
+++ bin/start.bat
@@ -1,4 +1,8 @@
 @echo off
+set "Y=%date:~0,4%"
+set "M=%date:~5,2%"
+set "D=%date:~8,2%"
+set "LOG=log\%Y%%M%%D%.log"
 set "JAVA_CMD=%JAVA_HOME%/bin/java"
 if "%JAVA_HOME%" == "" goto noJavaHome
 if exist "%JAVA_HOME%\bin\java.exe" goto mainEntry
@@ -9,4 +13,5 @@ echo ---------------------------------------------------
 set "JAVA_CMD=java"
 :mainEntry
 @echo on
-"%JAVA_CMD%" -jar abc-0.0.1-SNAPSHOT.war
+mkdir log
+"%JAVA_CMD%" -jar abc-0.0.1-SNAPSHOT.war > %LOG%
