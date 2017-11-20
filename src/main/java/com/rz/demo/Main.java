511ffd8499468f84176fb851f9aff1d295fa65e9
ruanzy <ruanzhiyong6496@163.com>
2017-11-20 09:15:40 +0800

    优化

--- src/main/java/com/rz/demo/Main.java
+++ src/main/java/com/rz/demo/Main.java
@@ -1,55 +1,11 @@
 package com.rz.demo;
 
-import java.io.File;
-import java.net.URL;
-import java.security.ProtectionDomain;
-
-import jone.web.Launcher;
-
-import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.webapp.WebAppContext;
+import jone.launcher.Launcher;
 
 public class Main
 {
 	public static void main(String[] args) throws Exception
 	{
-		Launcher.start(9090, "");
-	}
-	
-	public static void a(String[] args)
-	{
-		int port = 8080;
-		if (args.length > 0)
-		{
-			try
-			{
-				port = Integer.valueOf(args[0].toString());
-			}
-			catch (Exception e)
-			{
-				throw new RuntimeException(
-						"The server port must be an integer of 0-65535");
-			}
-		}
-		Server server = new Server(port);
-		server.setStopAtShutdown(true);
-		ProtectionDomain protectionDomain = Main.class.getProtectionDomain();
-		URL location = protectionDomain.getCodeSource().getLocation();
-		String warFile = location.toExternalForm();
-		WebAppContext context = new WebAppContext(warFile, "");
-		try
-		{
-			context.setServer(server);
-			String currentDir = new File(location.getPath()).getParent();
-			File workDir = new File(currentDir, "work");
-			context.setTempDirectory(workDir);
-			context.setExtraClasspath(currentDir + "/conf");
-			server.setHandler(context);
-			server.start();
-		}
-		catch (Exception e)
-		{
-			e.printStackTrace();
-		}
+		Launcher.run(9090, "");
 	}
 }
